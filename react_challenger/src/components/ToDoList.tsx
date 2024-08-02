dcf941664fc7caf4fa95b188ed174e8feddf9bd1
Lucas Dias <lucas_71194@hotmail.com>
2024-08-02 19:06:00 +0100

    Adicionar scroll bar +  inline cross after check

--- react_challenger/src/components/ToDoList.tsx
+++ react_challenger/src/components/ToDoList.tsx
@@ -8,27 +8,27 @@ function ToDoList() {
         <h1>To Do List</h1>
       </div>
       <li>
-        <label>
-          <ul>
-            <div className="toDoContent">
-              <input type="checkbox" className="checkbox" />
-              teste 1<span className="spanCross">&#10006;</span>
-            </div>
-          </ul>
-          <ul>
-            <div className="toDoContent">
-              <input type="checkbox" className="checkbox" />
-              teste 1<span className="spanCross">&#10006;</span>
-            </div>
-          </ul>
-        </label>
+        <ul>
+          <div className="toDoContent">
+            <input type="checkbox" className="checkbox" />
+            <span>teste 1</span>
+            <span className="spanCross">&#10006;</span>
+          </div>
+        </ul>
+        <ul>
+          <div className="toDoContent">
+            <input type="checkbox" className="checkbox" />
+            <span>teste 2</span>
+            <span className="spanCross">&#10006;</span>
+          </div>
+        </ul>
       </li>
-      <li>
-        <span>&#10010;</span>
-        Add new Task
-      </li>
+      <button>
+        <span>&#10010;Add new Task </span>
+      </button>
     </div>
   );
 }
 
 export default ToDoList;
+//  commit -m 'React App Create and add ToDo List component with html and css'
